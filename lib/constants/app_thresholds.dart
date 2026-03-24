e0797fb09eeb7bdd5c6b01eec6bfeaf0b540c811
lux228 <stuartos228@gmail.com>
2026-03-24 22:39:28 +0100

    fix: corriger les valeurs des seuils de temps pour les retards

--- lib/constants/app_thresholds.dart
+++ lib/constants/app_thresholds.dart
@@ -1,5 +1,5 @@
 class AppThresholds {
   static const int lateMinutes = -10;
-  static const int slightlyLateMinutes = 0;
-  static const int comingSoonMinutes = 15;
+  static const int slightlyLateMinutes = -5;
+  static const int comingSoonMinutes = 10;
 }
